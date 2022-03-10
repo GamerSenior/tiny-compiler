0658496b07d43430af18f9febdb596286260f7ec
Giovani Garcia <abelgiovani@gmail.com>
2022-03-10 13:48:59 -0300

    Testing lexer source output

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,15 @@
 const std = @import("std");
+const Lexer = @import("lexer.zig");
 
 pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+    const input = "LET foobar = 32";
+    var lexer = Lexer.init(input);
+
+    while (lexer.peek() != '\x00') {
+        std.log.info("{c}", .{lexer.current_char});
+        lexer.nextChar();
+    }
+    std.log.info("{s}", .{lexer.source});
 }
 
 test "basic test" {
